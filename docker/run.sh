869ccce2e9b813955eb508d041b404494fcadbb3
wang_mm <Qq6492178>
2017-08-08 10:52:29 +0800

    1.移除无用依赖 2.修改脚本编码格式

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-echo "服务启动"
+echo "清理服务"
+docker-compose down
+sleep 5s
+echo "后台管理系统启动"
+docker-compose -f docker-compose.yml up -d redis mongodb rabbitmq control-manger
+sleep 30s
 docker-compose -f docker-compose.yml up
+echo "配置中心启动"
