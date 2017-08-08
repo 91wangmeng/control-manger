80b6f3b9f5296b45e987f25ac0a83e4d5ea5070e
wang_mm <Qq6492178>
2017-08-08 20:23:21 +0800

    1.BaseEntity增加swagger注解支持

--- control-manger-common/src/main/java/com/drore/cloud/control/manger/common/base/domain/BaseEntity.java
+++ control-manger-common/src/main/java/com/drore/cloud/control/manger/common/base/domain/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.drore.cloud.control.manger.common.base.domain;
 
 import com.alibaba.fastjson.annotation.JSONField;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
@@ -17,12 +19,15 @@ import java.io.Serializable;
  */
 @Data
 @ToString
+@ApiModel
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 7764802712795360005L;
     @JSONField(name = "id")
     @Id
+    @ApiModelProperty(value = "id",notes = "数据id")
     private String id;
     @JSONField(name = "create_time")
+    @ApiModelProperty(hidden = true)
     private long createTime;
 
 }
