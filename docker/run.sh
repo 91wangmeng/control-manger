e42887785a658e2d4a37dad43c67de84c49eee98
wang_mm <Qq6492178>
2017-08-08 19:35:54 +0800

    1.docker脚本优化

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-echo "清理服务"
-docker-compose stop
-sleep 10s
 echo "后台管理系统启动"
-docker-compose -f docker-compose.yml up  redis mongodb rabbitmq control-manger
+docker-compose -f docker-compose.yml up -d redis mongodb rabbitmq control-manger
 sleep 30s
-docker-compose -f docker-compose.yml up
+docker-compose -f docker-compose.yml up -d control-manger-config
 echo "配置中心启动"
