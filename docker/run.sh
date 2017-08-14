e091ff8d55e1f09f0f283e85c756d47229b4fab9
wang_mm <Qq6492178>
2017-08-15 00:48:31 +0800

    1.增加teamCity

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "后台管理系统启动"
-docker-compose -f docker-compose.yml up -d redis mongodb rabbitmq control-manger
+docker-compose -f docker-compose.yml up -d redis mongodb rabbitmq teamcity-server control-manger
 sleep 30s
-docker-compose -f docker-compose.yml up -d control-manger-config
+docker-compose -f docker-compose.yml up -d control-manger-config teamcity-agent
 echo "配置中心启动"
