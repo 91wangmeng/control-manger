d504efe3ba0cd54ef3d51b33a0dbfc36cab69c15
wang_mm <Qq6492178>
2017-08-29 15:11:32 +0800

    1.缓存key统一增加景区前缀作为区分

--- control-manger-common/src/main/java/com/drore/cloud/control/manger/common/cache/service/impl/RedisBuilderImpl.java
+++ control-manger-common/src/main/java/com/drore/cloud/control/manger/common/cache/service/impl/RedisBuilderImpl.java
@@ -3,6 +3,7 @@ package com.drore.cloud.control.manger.common.cache.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.drore.cloud.control.manger.common.cache.service.RedisBuilder;
 import com.drore.cloud.control.manger.common.cache.utils.FastJsonRedisSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -21,28 +22,37 @@ import java.util.List;
 @Component
 public class RedisBuilderImpl implements RedisBuilder {
 
+    @Value("${scenic.env}")
+    private String scenic_env;
 
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
-
+    /**
+     * 为了区分缓存所属景区,所有key统一增加景区前缀
+     * @param key
+     * @return
+     */
+    private  String getKey(String key) {
+        return scenic_env +"_"+ key;
+    }
     @Override
     public void lPush(String key, Object value) {
         redisTemplate.execute((RedisCallback<Long>) connection ->
-                connection.lPush(key.getBytes(), JSON.toJSONBytes(value)));
+                connection.lPush(getKey(key).getBytes(), JSON.toJSONBytes(value)));
     }
 
     @Override
     public List<Object> lRange(String key, long start, long end) {
         return redisTemplate.execute((RedisCallback<List<Object>>) connection ->
                 new FastJsonRedisSerializer()
-                        .deserializeList(connection.lRange(key.getBytes(), start, end)));
+                        .deserializeList(connection.lRange(getKey(key).getBytes(), start, end)));
     }
 
     @Override
     public void set(String key, Object value) {
         redisTemplate.execute((RedisCallback) connection -> {
-            connection.set(key.getBytes(), JSON.toJSONBytes(value));
+            connection.set(getKey(key).getBytes(), JSON.toJSONBytes(value));
             return null;
         });
     }
@@ -50,13 +60,13 @@ public class RedisBuilderImpl implements RedisBuilder {
     @Override
     public void setNX(String key, Object value) {
         redisTemplate.execute((RedisCallback<Boolean>) connection ->
-                connection.setNX(key.getBytes(), JSON.toJSONBytes(value)));
+                connection.setNX(getKey(key).getBytes(), JSON.toJSONBytes(value)));
     }
 
     @Override
     public void setEX(String key, Object value, long seconds) {
         redisTemplate.execute((RedisCallback) connection -> {
-            connection.setEx(key.getBytes(), seconds, JSON.toJSONBytes(value));
+            connection.setEx(getKey(key).getBytes(), seconds, JSON.toJSONBytes(value));
             return null;
         });
     }
@@ -64,6 +74,6 @@ public class RedisBuilderImpl implements RedisBuilder {
     @Override
     public Object get(String key) {
         return redisTemplate.execute((RedisCallback<Object>) connection ->
-                new FastJsonRedisSerializer().deserialize(connection.get(key.getBytes())));
+                new FastJsonRedisSerializer().deserialize(connection.get(getKey(key).getBytes())));
     }
 }
