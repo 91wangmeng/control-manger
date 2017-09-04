62d307767c03ede9204cf4f2f662f3ba547aad32
wang_mm <Qq6492178>
2017-09-04 14:22:05 +0800

    1.增加全局缓存相关方法

--- control-manger-common/src/main/java/com/drore/cloud/control/manger/common/cache/service/RedisBuilder.java
+++ control-manger-common/src/main/java/com/drore/cloud/control/manger/common/cache/service/RedisBuilder.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 /**
  * 浙江卓锐科技股份有限公司 版权所有 © Copyright 2017<br/>
- * 说明: <br/>
+ * 说明: 带global为全局缓存<br/>
  * 项目名称: control-manger <br/>
  * 创建日期: 2017年07月23日 21:03 <br/>
  * 作者: <a href="6492178@gmail.com">汪萌萌</a>
@@ -19,6 +19,14 @@ public interface RedisBuilder {
     void lPush(String key, Object value);
 
     /**
+     * Global l push.
+     *
+     * @param key   the key
+     * @param value the value
+     */
+    void globalLPush(String key, Object value);
+
+    /**
      * L range list.
      *
      * @param key   the key
@@ -29,6 +37,16 @@ public interface RedisBuilder {
     List<Object> lRange(String key, long start, long end);
 
     /**
+     * Global l range list.
+     *
+     * @param key   the key
+     * @param start the start
+     * @param end   the end
+     * @return the list
+     */
+    List<Object> globalLRange(String key, long start, long end);
+
+    /**
      * Set.
      *
      * @param key   the key
@@ -37,12 +55,28 @@ public interface RedisBuilder {
     void set(String key, Object value);
 
     /**
+     * Sets global.
+     *
+     * @param key   the key
+     * @param value the value
+     */
+    void setGlobal(String key, Object value);
+
+    /**
      * Sets nx.
      *
      * @param key   the key
      * @param value the value
      */
     void setNX(String key, Object value);
+
+    /**
+     * Sets global nx.
+     *
+     * @param key   the key
+     * @param value the value
+     */
+    void setGlobalNX(String key, Object value);
 
     /**
      * Sets ex.
@@ -54,6 +88,15 @@ public interface RedisBuilder {
     void setEX(String key, Object value, long seconds);
 
     /**
+     * Sets global ex.
+     *
+     * @param key     the key
+     * @param value   the value
+     * @param seconds the seconds
+     */
+    void setGlobalEX(String key, Object value, long seconds);
+
+    /**
      * Get object.
      *
      * @param key the key
@@ -61,7 +104,43 @@ public interface RedisBuilder {
      */
     Object get(String key);
 
+    /**
+     * Gets global.
+     *
+     * @param key the key
+     * @return the global
+     */
+    Object getGlobal(String key);
+
+    /**
+     * Exit key boolean.
+     *
+     * @param key the key
+     * @return the boolean
+     */
     boolean exitKey(String key);
 
+    /**
+     * Exit global key boolean.
+     *
+     * @param key the key
+     * @return the boolean
+     */
+    boolean exitGlobalKey(String key);
+
+    /**
+     * Del key long.
+     *
+     * @param key the key
+     * @return the long
+     */
     Long delKey(String key);
+
+    /**
+     * Del global key long.
+     *
+     * @param key the key
+     * @return the long
+     */
+    Long delGlobalKey(String key);
 }
--- control-manger-common/src/main/java/com/drore/cloud/control/manger/common/cache/service/impl/RedisBuilderImpl.java
+++ control-manger-common/src/main/java/com/drore/cloud/control/manger/common/cache/service/impl/RedisBuilderImpl.java
@@ -40,52 +40,92 @@ public class RedisBuilderImpl implements RedisBuilder {
 
     @Override
     public void lPush(String key, Object value) {
+        globalLPush(getKey(key), value);
+    }
+
+    @Override
+    public void globalLPush(String key, Object value) {
         redisTemplate.execute((RedisCallback<Long>) connection ->
-                connection.lPush(getKey(key).getBytes(), JSON.toJSONBytes(value)));
+                connection.lPush(key.getBytes(), JSON.toJSONBytes(value)));
     }
 
     @Override
     public List<Object> lRange(String key, long start, long end) {
+        return globalLRange(getKey(key), start, end);
+    }
+
+    @Override
+    public List<Object> globalLRange(String key, long start, long end) {
         return redisTemplate.execute((RedisCallback<List<Object>>) connection ->
                 new FastJsonRedisSerializer()
-                        .deserializeList(connection.lRange(getKey(key).getBytes(), start, end)));
+                        .deserializeList(connection.lRange(key.getBytes(), start, end)));
     }
 
     @Override
     public void set(String key, Object value) {
+        setGlobal(getKey(key), value);
+    }
+
+    @Override
+    public void setGlobal(String key, Object value) {
         redisTemplate.execute((RedisCallback) connection -> {
-            connection.set(getKey(key).getBytes(), JSON.toJSONBytes(value));
+            connection.set(key.getBytes(), JSON.toJSONBytes(value));
             return null;
         });
     }
 
     @Override
     public void setNX(String key, Object value) {
+        setGlobalNX(getKey(key), JSON.toJSONBytes(value));
+    }
+
+    @Override
+    public void setGlobalNX(String key, Object value) {
         redisTemplate.execute((RedisCallback<Boolean>) connection ->
-                connection.setNX(getKey(key).getBytes(), JSON.toJSONBytes(value)));
+                connection.setNX(key.getBytes(), JSON.toJSONBytes(value)));
     }
 
     @Override
     public void setEX(String key, Object value, long seconds) {
+        setGlobalEX(getKey(key), value, seconds);
+    }
+
+    @Override
+    public void setGlobalEX(String key, Object value, long seconds) {
         redisTemplate.execute((RedisCallback) connection -> {
-            connection.setEx(getKey(key).getBytes(), seconds, JSON.toJSONBytes(value));
+            connection.setEx(key.getBytes(), seconds, JSON.toJSONBytes(value));
             return null;
         });
     }
 
     @Override
     public Object get(String key) {
+        return getGlobal(getKey(key));
+    }
+
+    @Override
+    public Object getGlobal(String key) {
         return redisTemplate.execute((RedisCallback<Object>) connection ->
-                new FastJsonRedisSerializer().deserialize(connection.get(getKey(key).getBytes())));
+                new FastJsonRedisSerializer().deserialize(connection.get(key.getBytes())));
     }
 
     @Override
     public boolean exitKey(String key) {
-        return redisTemplate.execute((RedisCallback<Boolean>) connection -> connection.exists(getKey(key).getBytes()));
+        return exitGlobalKey(getKey(key));
+    }
+
+    @Override
+    public boolean exitGlobalKey(String key) {
+        return redisTemplate.execute((RedisCallback<Boolean>) connection -> connection.exists(key.getBytes()));
     }
 
     @Override
     public Long delKey(String key) {
-        return redisTemplate.execute((RedisCallback<Long>) connection -> connection.del(getKey(key).getBytes()));
+        return delGlobalKey(getKey(key));
+    }
+
+    @Override
+    public Long delGlobalKey(String key) {
+        return redisTemplate.execute((RedisCallback<Long>) connection -> connection.del(key.getBytes()));
     }
 }
